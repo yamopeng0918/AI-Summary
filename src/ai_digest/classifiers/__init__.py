413b4a9ad4bc18d6e77ada37370a9af2daebdeab
yamopeng0918 <pengym@gmail.com>
2026-08-09 15:46:17 +0800

    Revert "feat: orchestrate article summarization"
    This reverts commit aec254629f24d3b98d757e10cb1cefc59ca6e716.

--- src/ai_digest/classifiers/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Text classification adapters."""
-
-from ai_digest.classifiers.base import Classifier
-from ai_digest.classifiers.fixed import FixedClassifier
-
-__all__ = ["Classifier", "FixedClassifier"]
--- src/ai_digest/summarizers/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Summary generation adapters."""
-
-from ai_digest.summarizers.base import Summarizer
-from ai_digest.summarizers.openai import OpenAISummarizer
-
-__all__ = ["OpenAISummarizer", "Summarizer"]
